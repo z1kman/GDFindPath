0793965a17d77011d573f90e015cf709a90ce530
z1kman <z1kmanfast@gmail.com>
2023-12-28 18:17:48 +0300

    updated

--- CameraHolder.gd
+++ CameraHolder.gd
@@ -10,16 +10,17 @@ func  _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		# Движение камеры по x и y
 		if event.button_mask == MOUSE_BUTTON_RIGHT:
+			# Поворот камеры по X
 			_camera.rotate_x(deg_to_rad(event.relative.y * mouse_sensitivity))
+			# Поворот CameraHolder по Y
 			rotate_y(deg_to_rad(event.relative.x * mouse_sensitivity))
-			_camera.rotation_degrees.x = clamp(_camera.rotation_degrees.x, -89.9, 89.9)
 	# Скролл колесиками мыши
 	elif event is InputEventMouseButton:
 		# Зум - 
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			var newVector = Vector3(0, 0, mouse_zoom_step)
-			_camera.translate(newVector)
+			translate(newVector)
 		# Зум +
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP: 
 			var newVector = Vector3(0, 0, -mouse_zoom_step)
-			_camera.translate(newVector)
+			translate(newVector)
